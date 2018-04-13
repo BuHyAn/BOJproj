649e3e0d03e4505db067031832089606d6a1748b
annbang <adh0463@naver.com>
2018-04-13 22:39:20 +0900

    20180413 house

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
+
 int main() {
 	freopen("input.txt", "r", stdin);
-	int T, W, H;
-	scanf("%d", &T);
+	char N[81];
+	int	T;
+	scanf("%d\n", &T);
 	while (T--) {
-		scanf("%d%d", &H, &W);
+		scanf("%s", N);
+		if (std::next_permutation(N, N + strlen(N))) printf("%s\n", N);
+		else printf("BIGGEST\n");
 	}
 	return 0;
 }
