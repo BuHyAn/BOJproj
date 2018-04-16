90cc53539249f5c79b5bb1f9159b8de7671dd6ea
annbang <adh0463@naver.com>
2018-04-16 22:26:38 +0900

    20180416 house

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <cstdio>
-int max_v(int a, int b) { return a > b ? a : b; }
-int N, M, board[8][8], cctv_p[8][2], ctv_c, dir_cnt[] = { 4,2,4,4,1 };
+int abs_v(int a) { return a < 0 ? -a : a; }
+int min_v(int a, int b) { return a < b ? a : b; }
+int N, board[20][20],S;
 
-
-int rec(int p) {
-	if (p == ctv_c) {
-
-	}
+int rec(int p, int s) {
+	if (p == N) return abs_v(S - 2*s);
 	
+	return min_v()
 }
 int main() {
 	freopen("input.txt", "r", stdin);
@@ -15,16 +14,9 @@ int main() {
 	char A[1001], B[1001];
 	scanf("%s%s", A, B);
 	*/
-	int i, j;
-	scanf("%d%d", &N, &M);
-	for (i = 0; i < N; i++) for (j = 0; j < M; j++) {
-		scanf("%d", &board[i][j]);
-		if (board[i][j] && board[i][j] != 6) {
-			cctv_p[ctv_c][0] = i, cctv_p[ctv_c][1] = j;
-			ctv_c++;
-		}
-	}
-	printf("%d", N*M - rec(0));
-
+	int i, j, S=0;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++) for (j = 0; j < N; j++) scanf("%d", &board[i][j]), S += board[i][j];
+	
 	return 0;
 }
